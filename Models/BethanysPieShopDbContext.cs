aa2c3935ba375d8048097643255a2a7064af16ca
Gökhan Ulutaş <gokhan.ulutas@roofstacks.com>
2025-04-14 22:31:16 +0300

    update database initial migration

---
+++ Models/BethanysPieShopDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShopAdmin.Models;
+
+public class BethanysPieShopDbContext : DbContext
+{
+    public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Pie> Pies { get; set; } 
+    public DbSet<Category> Categories { get; set; } 
+    public DbSet<Order> Orders { get; set; } 
+    public DbSet<OrderDetail> OrderDetails { get; set; } 
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //configurations
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BethanysPieShopDbContext).Assembly);// Apply all configurations in the assembly
+        
+        modelBuilder.Entity<Pie>().ToTable("Pies");
+        modelBuilder.Entity<Category>().ToTable("Categories");
+        modelBuilder.Entity<Order>().ToTable("Orders");
+        modelBuilder.Entity<OrderDetail>().ToTable("OrderDetails");
+        
+        //configuring using Fluent API
+        modelBuilder.Entity<Category>()
+            .Property(p => p.Name)
+            .IsRequired();
+    }
+     
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 
+using BethanysPieShopAdmin.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BethanysPieShopDbContextConnection")));
 
 
 var app = builder.Build();
